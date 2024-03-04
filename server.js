38d3b933d535f5b6ee749cbc3fe8fa004fd14bda
Sai Vimal Goda <156933401+sai-goda123@users.noreply.github.com>
2024-03-03 20:41:06 -0500

    worked on mongodb

---
+++ server.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const budgetModel = require("./mongo/pbudget");
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+};
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use("/", express.static("public"));
+
+app.get("/info", (req, res) => {
+  mongoose.connect("mongodb://127.0.0.1:27017/piechartdata", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the Database");
+    budgetModel.find({})
+      .then((data) => {
+        res.json(data);
+        console.log(data);
+        mongoose.connection.close();
+      })
+      .catch((connectionError) => {
+        console.error(connectionError);
+      });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+});
+
+app.post("/info", (req, res) => {
+  mongoose.connect("mongodb://127.0.0.1:27017/piechartdata", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database");
+    const newItem = new budgetModel(req.body);
+    budgetModel.create(newItem) 
+      .then((data) => {
+        res.json(data);
+        console.log(data);
+        mongoose.connection.close();
+      })
+      .catch((connectionError) => {
+        console.error(connectionError);
+        res.status(400).json({error:'Internal Server error-Validation failed'})
+      });
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(400).json({error:'Internal Server error'})
+  });
+});
+
+app.listen(port, () => {
+  console.log(`API served at http://localhost:${port}`);
+});
